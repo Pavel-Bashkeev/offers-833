a686c6e157f6ef477eab384a277e5535b94ac84e
Pavel-Bashkeev <pavelbashkeev@gmail.com>
2021-11-22 09:22:38 +0300

    offers-833

---
+++ app/js/vendor/slider.js
@@ -0,0 +1,11 @@
+const slider = new Swiper('.swiper', {
+  slidePreView: 1,
+  spaceBetween: 20,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+  
+  autoHeight: true
+})
